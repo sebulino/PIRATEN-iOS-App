cafb508aa469b1b04e13ea8f4d106c62a2b504f8
Sebastian <sebastian.alscher@gmail.com>
2026-01-29 18:31:56 +0100

    Initial Commit

---
+++ PIRATENTests/PIRATENTests.swift
@@ -0,0 +1,17 @@
+//
+//  PIRATENTests.swift
+//  PIRATENTests
+//
+//  Created by Sebulino on 29.01.26.
+//
+
+import Testing
+@testable import PIRATEN
+
+struct PIRATENTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
